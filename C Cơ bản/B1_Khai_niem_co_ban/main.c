a17810707fbba3fed58ff73f01db20d855c8008f
Annguyen011 <nguyenvanan01102003@gmail.com>
2026-04-21 20:47:50 +0700

    add

--- C Cơ bản/B1_Khai_niem_co_ban/main.c
+++ C Cơ bản/B1_Khai_niem_co_ban/main.c
@@ -1,6 +1,13 @@
 ﻿#include<stdio.h>
 
+#define PI 3.14
+#define CONG(a, b) (a + b)
+// macro chỉ là thay thế kh kiểm tra lỗi
+// const là hằng số có kiểu dữ liệu, có thể kiểm tra lỗi
 int main() {
     printf("Hello world!");
+    printf("\nPI = %f", PI);
+    printf("\nCong 2 va 3 = %d", CONG(2, 3));
+    printf("\n");
     return 0;
 }
