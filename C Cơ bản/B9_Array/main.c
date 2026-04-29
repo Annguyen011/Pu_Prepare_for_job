34e04f3c9f4b2b0b10e96929077bbfc6caf8dd18
Annguyen011 <nguyenvanan01102003@gmail.com>
2026-04-30 04:11:21 +0700

    add functions to input and print array elements

--- C Cơ bản/B9_Array/main.c
+++ C Cơ bản/B9_Array/main.c
@@ -1,5 +1,8 @@
 ﻿#include<stdio.h>
 
+void nhap_mang(int a[], int n);
+void in_mang(int a[], int n);
+
 int	main(int argc, char **argv)
 {
     int arr[5] = {1, 2, 3, 4, 5};
@@ -11,4 +14,21 @@ int	main(int argc, char **argv)
 
 
     return 0;
+}
+
+void nhap_mang(int a[], int n) 
+{
+    for (int i = 0; i < n; i++) {
+        printf("Nhap phan tu thu %d: ", i);
+        scanf("%d", &a[i]);
+    }
+    in_mang(a, n);
+}
+
+void in_mang(int a[], int n) 
+{
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
 }
