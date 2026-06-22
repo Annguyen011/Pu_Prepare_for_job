730e45445f3d213d2c9727d9a26080202abd0a45
Annguyen011 <nguyenvanan01102003@gmail.com>
2026-06-22 20:51:59 +0700

    add

--- C Nang cao/B2_Mang/main.c
+++ C Nang cao/B2_Mang/main.c
@@ -7,6 +7,18 @@ void in_mang(int *pta, int n)
     {
         printf("%d ", *(pta + i));
     }
+
+}
+
+void nhap_mang(int *ptarr, int *so_phan_tu)
+{
+    printf("Nhap so phan tu: ");
+    scanf("%d", so_phan_tu);
+    for(int i = 0; i < *so_phan_tu; i++)
+    {
+        printf("Nhap phan tu thu %d: ", i + 1);
+        scanf("%d", (ptarr + i));
+    }
 }
 
 int main() {
