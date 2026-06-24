e4c22e0a81b4e6990dec079d603cf7faa78cf49d
Annguyen011 <nguyenvanan01102003@gmail.com>
2026-06-25 06:23:01 +0700

    add

--- C++ Cau truc du lieu/B2/main.cpp
+++ C++ Cau truc du lieu/B2/main.cpp
@@ -2,21 +2,24 @@
 
 using namespace std;
 
+void vi_du_sort()
+{
+    int a[10] = {1, 2, 3, 4, 5, 4, 9, 7};
+    int n = 10;
+    sort(a, a + n);
+    for(auto x:a)
+    {
+        cout << x << " ";
+    }
+
+    char c[10] = "Hello";
+    cout << c << endl;
+    sort(c, c + 5);
+    cout << c << endl;
+}
+
 int main(int argc, char const *argv[])
 {
-    // int a[10] = {1, 2, 3, 4, 5, 4, 9, 7};
-    // int n = 10;
-    // sort(a, a + n);
-    // for(auto x:a)
-    // {
-    //     cout << x << " ";
-    // }
-
-    // char c[10] = "Hello";
-    // cout << c << endl;
-    // sort(c, c + 5);
-    // cout << c << endl;
-
     
     return 0;
 }
