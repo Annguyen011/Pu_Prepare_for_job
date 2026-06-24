a6031d8c75bd5080925782374a041102f7c9180f
annguyen011 <nguyenvanan01102003@gmail.com>
2026-06-24 16:12:56 +0700

    add

--- C++ Cau truc du lieu/B1/main.cpp
+++ C++ Cau truc du lieu/B1/main.cpp
@@ -22,7 +22,35 @@ int main()
     set<int> s = {1, 2, 3, 4, 5, 6, 7};
     cout << *s.begin() << endl; // In ra giá trị của phần tử đầu
     cout << *s.rbegin() << endl; // In ra giá trị của phần tử
+    cout << endl;
+    for(auto it = s.begin(); it != s.end(); it++)
+    {
+        cout << *it << " "; // In ra giá trị mà con trỏ iterator đang trỏ tới
+    }
+    cout << endl;
+    for(auto it = s.rbegin(); it != s.rend(); it++)
+    {
+        cout << *it << " "; // In ra giá trị mà con trỏ iterator đang trỏ tới
+    }
 
+    if(auto it = s.find(5); it != s.end())
+    {
+        cout << endl << "Tim thay 5" << endl;
+    }
+    else
+    {
+        cout << endl << "Khong tim thay 5" << endl;
+    }
+
+    if(s.count(5) > 0)
+    {
+        s.erase(5); // Xóa phần tử có giá trị 5
+        cout << "Da xoa 5" << endl;
+    }
+    else
+    {
+        cout << "Khong tim thay 5" << endl;
+    }
     
     return 0;
 }
