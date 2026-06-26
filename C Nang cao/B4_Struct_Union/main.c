c81237912611c2c2997d4e78be6d6352e12932c4
annguyen011 <nguyenvanan01102003@gmail.com>
2026-06-27 06:54:51 +0700

    add

--- C Nang cao/B4_Struct_Union/main.c
+++ C Nang cao/B4_Struct_Union/main.c
@@ -1,20 +1,25 @@
 #include<stdio.h>
 
+void con_tro_hang_va_hang_con_tro() {
+    // Khai báo một hằng số
+    const int NAM_HOC = 2026;
+
+    // Khai báo một con trỏ hằng
+    const int *ptr_hang = &NAM_HOC;
+
+    // Khai báo một hằng con trỏ
+    int *const ptr_con_tro = &NAM_HOC;
+
+    // Khai báo một hằng con trỏ hằng
+    const int *const ptr_hang_con_tro = &NAM_HOC;
+
+    printf("Giá trị của NAM_HOC: %d\n", NAM_HOC);
+    printf("Giá trị thông qua con trỏ hằng: %d\n", *ptr_hang);
+    printf("Giá trị thông qua hằng con trỏ: %d\n", *ptr_con_tro);
+    printf("Giá trị thông qua hằng con trỏ hằng: %d\n", *ptr_hang_con_tro);
+}
+
 int main() {
-    // Khai báo một biến kiểu struct
-    struct SinhVien {
-        char ten[50];
-        int tuoi;
-        float diemTB;
-    };
-
-    // Khởi tạo một biến kiểu struct SinhVien
-    struct SinhVien sv1 = {"Nguyen Van A", 20, 8.5};
-
-    // In thông tin của sinh viên
-    printf("Ten: %s\n", sv1.ten);
-    printf("Tuoi: %d\n", sv1.tuoi);
-    printf("Diem TB: %.2f\n", sv1.diemTB);
 
     return 0;
 }
