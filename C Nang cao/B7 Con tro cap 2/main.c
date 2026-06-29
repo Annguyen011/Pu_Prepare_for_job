e2e0a9d9a524efff2464092a8d778f3ca151e217
annguyen011 <nguyenvanan01102003@gmail.com>
2026-06-29 14:44:01 +0700

    add

---
+++ C Nang cao/B7 Con tro cap 2/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void bai_tap_1()
+{
+    int x = 100;
+    int *ptr1 = &x; // Assign the value of x to ptr1
+    printf("Value of ptr1: %d\n", ptr1);
+    int **ptr2 = &ptr1;
+
+    printf("Value of ptr2: %d\n", ptr2);
+    
+}
+
+int main()
+{
+
+
+    return 0;
+}
