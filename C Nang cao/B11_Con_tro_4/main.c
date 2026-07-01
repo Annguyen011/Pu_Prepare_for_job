24173ca3c465e96914c83b1ba1e2d4e6abfedf90
Annguyen011 <nguyenvanan01102003@gmail.com>
2026-07-01 15:28:52 +0700

    add

---
+++ C Nang cao/B11_Con_tro_4/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+void bat_quat()
+{
+    printf("Bat_quat\n");
+}
+
+void bat_den()
+{
+    printf("Bat_den\n");
+}
+
+
+
+int main()
+{
+
+
+
+    return 0;
+}
