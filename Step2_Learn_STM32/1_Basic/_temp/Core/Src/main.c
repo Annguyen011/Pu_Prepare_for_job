0582ad42a8459053a0cb2635824be17e01030c14
annguyen011 <nguyenvanan01102003@gmail.com>
2026-07-13 05:56:55 +0700

    add

--- Step2_Learn_STM32/1_Basic/_temp/Core/Src/main.c
+++ Step2_Learn_STM32/1_Basic/_temp/Core/Src/main.c
@@ -95,8 +95,6 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-      HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-      HAL_Delay(1000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
